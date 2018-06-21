6be7471b5579c441926fdda33b6fcc02b82f3686
Imran Yousuf <imraneecs@gmail.com>
2018-06-21 00:13:56 -0700

    list view works

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ListView} from 'react-native';
+import { connect } from 'react-redux';
+import PersonItem from './PersonItem';
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: 353,
+        flexWrap: 'wrap',
+        paddingTop: 20,
+        paddingLeft: 20,
+    },
+});
 
 
 
 
-export default class PeopleList extends Component<Props> {
+
+
+class PeopleList extends Component<Props> {
+
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1,r2) => r1 !== r2,
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.people);
+    }
+
     render() {
         return (
-            <View >
-                <Text> I see People </Text>
+            <View style={styles.container}>
+                <ListView
+                    enableEmptySections={true}
+                    dataSource={this.dataSource}
+                    renderRow={(rowData) =>
+                        <PersonItem people={rowData} />
+                    }
+                    />
+
             </View>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+  return { people: state.people};
+};
+
+export default connect(mapStateToProps)(PeopleList);
--- src/reducers/peopleReducer.js
+++ src/reducers/peopleReducer.js
@@ -1,10 +1,10 @@
 import people from './people.json';
 
 const initialState ={
-    peope: people,
+    people: people,
 };
 
-export default (state= initialState, action) => {
+export default (state=initialState, action) => {
     switch(action.type){
         default:
             return state;
